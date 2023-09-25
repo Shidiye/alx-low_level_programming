a3bbe052f2672e6871912556227a8a4e1fb26194
Shidiye <ataheymaestro12@gmail.com>
2023-09-25 07:06:29 -0700

    5-strstr

---
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that checks the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *s = "hello, world";
+	char *f = "world";
+	char *t;
+
+	t = _strstr(s, f);
+	printf("%s\n", t);
+	return (0);
+}
+
